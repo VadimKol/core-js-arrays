5b7165aa3c4c9f2b784994dce565b221c021e963
Vadim Kolymbet <3t0n@inbox.ru>
2024-01-31 11:09:17 +0300

    feat: solve getAverage task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -137,8 +137,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return arr.length
+    ? Number((arr.reduce((a, b) => a + b) / arr.length).toFixed(2))
+    : 0;
 }
 
 /**
