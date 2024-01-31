5249bac3a5f006ee117f56603cf627f1f07ce9c0
Vadim Kolymbet <3t0n@inbox.ru>
2024-01-31 17:14:15 +0300

    feat: solve shiftArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -583,8 +583,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return n > 0
+    ? arr.slice(-n).concat(arr.slice(0, arr.length - n))
+    : arr.slice(-1 * (arr.length - n * -1)).concat(arr.slice(0, n * -1));
 }
 
 /**
