f6b69f86cdacd1c6360fff1cf1a1ec6cb5137e76
Vadim Kolymbet <3t0n@inbox.ru>
2024-01-31 15:32:50 +0300

    feat: solve getIndicesOfOddNumbers task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -459,8 +459,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((el, index) => (el % 2 ? index : Math.PI))
+    .filter((x) => x !== Math.PI);
 }
 
 /**
